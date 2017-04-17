9022cf95bb52e72bbaed7820c5df615d6ca2fff5
OSTROVSKYI\Альберт <ostrovskiyan15@gmail.com>
2017-04-18 01:25:27 +0300

    issue headers style

---
+++ src/issues/IssueTimeHeader.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import {Badge, Col, Row} from "react-bootstrap";
+
+class IssueTimeHeader extends Component {
+
+    render() {
+        let dateLabel = this.props.startDate;
+        if(this.props.endDate) {
+            dateLabel += (" - " + this.props.endDate);
+        }
+        return (
+            <div className="IssueTimeHeader">
+                <span className="IssueTimeHeaderTitle">{this.props.title}</span>
+                <span className="IssueTimeHeaderDates">{dateLabel}</span>
+                <Badge className="IssueTimeHeaderCount" pullRight>{this.props.issueCount}</Badge>
+            </div>
+        )
+    }
+}
+
+export default IssueTimeHeader;
--- src/issues/Issues.js
+++ src/issues/Issues.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import {Col, Row} from "react-bootstrap";
+import "./Issues.css"
+import IssueTimeHeader from "./IssueTimeHeader";
 
 class Issues extends Component {
 
@@ -7,8 +9,14 @@ class Issues extends Component {
         return (
             <Row className="Main">
                 {this.props.menu}
-                <Col xs={5} className="FullHeight WorkFrame">second</Col>
-                <Col xs={5} className="FullHeight WorkFrame">third</Col>
+                <Col xs={5} className="FullHeight Content DoubleThirdContent">
+                    <IssueTimeHeader title="НА СЕГОДНЯ" startDate="Окт 09" issueCount={0}/>
+                </Col>
+                <Col xs={5} className="FullHeight Content ThirdContent">
+                    <IssueTimeHeader title="НА ЭТУ НЕДЕЛЮ" startDate="Окт 09" endDate="Окт 15" issueCount={0}/>
+                    <IssueTimeHeader title="НА СЛЕД. НЕДЕЛЮ" startDate="Окт 16" endDate="Окт 22" issueCount={0}/>
+                    <IssueTimeHeader title="ПОЗЖЕ" startDate="После Окт 22" issueCount={0}/>
+                </Col>
             </Row>
         )
     }
