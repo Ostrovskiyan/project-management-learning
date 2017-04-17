faad61f181b42b88e2343567b28e96029df8ca00
OSTROVSKYI\Альберт <ostrovskiyan15@gmail.com>
2017-04-17 13:22:47 +0300

    menu style

--- src/common/PageTemplate.js
+++ src/common/PageTemplate.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import "./Main.css";
 import AppNavbar from "./AppNavbar";
-import {Col, Row} from "react-bootstrap";
+import {Button, Col, Glyphicon, Row} from "react-bootstrap";
 
 class PageTemplate extends Component {
     render() {
@@ -9,7 +9,15 @@ class PageTemplate extends Component {
             <div className="Background">
                 <AppNavbar/>
                 <Row className="Main">
-                    <Col xs={2} className="FullHeight Menu">first</Col>
+                    <Col xs={2} className="FullHeight Menu">
+                        <div className="MenuButton Selected">
+                            МОЯ РАБОТА
+                        </div>
+                        <div className="MenuButton">
+                            ПРОЕКТЫ
+                            <Glyphicon className="ProjectPlus" glyph="glyphicon glyphicon-plus" />
+                        </div>
+                    </Col>
                     <Col xs={5} className="FullHeight WorkFrame">second</Col>
                     <Col xs={5} className="FullHeight WorkFrame">third</Col>
                 </Row>
--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
+// let store = createStore();
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    /*<Provider store={store}>
+        <App />
+    </Provider>*/
+    <App/>,
+    document.getElementById('root')
 );
