3a679ce1c1ced4d01a4df992990944691e5f4551
Albert Ostrovskyi <ostrovskiyan15@gmail.com>
2017-04-20 00:04:58 +0300

    advnced issue style

--- src/issues/IssueDoubleImage.js
+++ src/issues/IssueDoubleImage.js
@@ -1,5 +1,4 @@
 import React, {Component} from "react";
-import {Form, FormControl} from "react-bootstrap";
 
 class IssueDoubleImage extends Component {
 
--- src/issues/IssueTimeHeader.js
+++ src/issues/IssueTimeHeader.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component} from "react";
 import {Badge} from "react-bootstrap";
 
 class IssueTimeHeader extends Component {
