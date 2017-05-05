051de1e7c6caf4d63145d53a9b1fb899a70db078
Albert Ostrovskyi <a.ostrovskyi@infostroy.com.ua>
2017-05-05 20:30:03 +0300

    update issue

--- src/issues/update-issue-form/UpdateIssueDate.js
+++ src/issues/update-issue-form/UpdateIssueDate.js
@@ -1,12 +1,30 @@
 import React, {Component} from "react";
 import styles from "./UpdateIssue.css";
-import mainStyles from "../../common/Main.css";
+import {Form, FormControl, FormGroup, InputGroup} from "react-bootstrap";
 
 class UpdateIssueDate extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {daysFocus: false};
+        this.handleDaysFocus = this.handleDaysFocus.bind(this);
+        this.handleDaysBlur = this.handleDaysBlur.bind(this);
+    }
 
+    handleDaysFocus(event) {
+        event.preventDefault();
+        this.setState({
+            ...this.state,
+            daysFocus: true
+        });
+    }
+
+    handleDaysBlur(event) {
+        event.preventDefault();
+        this.setState({
+            ...this.state,
+            daysFocus: false
+        });
     }
 
     render() {
@@ -19,6 +37,41 @@ class UpdateIssueDate extends Component {
                     <div className={`${styles.Option} ${styles.Selected}`}>ЗАВТРА</div>
                     <div className={styles.Option}>НА СЛЕД. НЕДЕЛЕ.</div>
                     <div className={styles.Option}>ВЫБРАТЬ ДАТУ</div>
+                </div>
+                <div className={styles.Results}>
+                    <div className={styles.DateResult}>
+                        <div className={styles.ResultHeader}>
+                            Даты:
+                        </div>
+                        <div>
+                            <Form inline>
+                                <FormGroup className={styles.DateInputWrapper}>
+                                    <InputGroup>
+                                        <FormControl type="text" defaultValue="10/10/2016" className={styles.ResultInput}/>
+                                    </InputGroup>
+                                </FormGroup>
+                                <span className={styles.BetweenDates}> &#8211; </span>
+                                <FormGroup className={styles.DateInputWrapper}>
+                                    <InputGroup>
+                                        <FormControl type="text" defaultValue="10/10/2016" className={styles.ResultInput}/>
+                                    </InputGroup>
+                                </FormGroup>
+                            </Form>
+                        </div>
+                    </div>
+                    <div>
+                        <div className={styles.ResultHeader}>
+                            Длительность:
+                        </div>
+                        <div>
+                            <FormGroup>
+                                <InputGroup className={this.state.daysFocus ? styles.DaysFocus : styles.DaysBlur}>
+                                    <FormControl type="text" defaultValue="1" className={`${styles.Days} ${styles.ResultInput}`} onFocus={this.handleDaysFocus} onBlur={this.handleDaysBlur}/>
+                                    <InputGroup.Addon>день</InputGroup.Addon>
+                                </InputGroup>
+                            </FormGroup>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
